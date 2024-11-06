0f74a875c23506f3144d339ceba4616aa850eb44
WeiShiLindonArelius <116113152+WeiShiLindonArelius@users.noreply.github.com>
2024-11-06 11:32:06 -0600

    Update stat_functions.py

--- stat_functions.py
+++ stat_functions.py
@@ -1,7 +1,134 @@
+import sqlite3
+
 from scipy.stats import binomtest
 from colorama import Fore
 import numpy as np
-from Players import PlayerSeason, grade_seasons
+from Players import PlayerSeason, grade_seasons, NO_SQL
+import pandas as pd
+
+def QUERY(sql, connect=sqlite3.connect('ControlDataBase.db'), params=None, is_select=False):
+    if NO_SQL:
+        return 0
+    elif is_select:
+        if params:
+            return pd.read_sql_query(sql, connect, params=params)
+        else:
+            return pd.read_sql_query(sql, connect)
+    else:
+        cur = connect.cursor()
+        if params:
+            cur.execute(sql, params)
+        else:
+            cur.execute(sql)
+        connect.commit()
+        return cur.lastrowid
+
+def clear_all_databases():
+    connect = sqlite3.connect('ControlDataBase.db')
+    query = "DROP TABLE IF EXISTS Game;"
+    QUERY(query, connect, is_select=False)
+
+    query = "DROP TABLE IF EXISTS Match;"
+    QUERY(query, connect, is_select=False)
+
+    query = "DROP TABLE IF EXISTS Series;"
+    QUERY(query, connect, is_select=False)
+
+    query = "DROP TABLE IF EXISTS Team;"
+    QUERY(query, connect, is_select=False)
+
+    query = "DROP TABLE IF EXISTS Player;"
+    QUERY(query, connect, is_select=False)
+
+    query = "DROP TABLE IF EXISTS TotalSeason;"
+    QUERY(query, connect, is_select=False)
+
+    query = "DROP TABLE IF EXISTS TeamSeason"
+    QUERY(query, connect, is_select=False)
+def initiate_databases():
+    connect = sqlite3.connect("ControlDataBase.db")
+    query = """ CREATE TABLE IF NOT EXISTS Team (
+                team_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                team_name TEXT,
+                region_of_origin TEXT,
+                season_of_origin INTEGER)
+    """
+    QUERY(query, connect, is_select=False)
+    query = """ CREATE TABLE IF NOT EXISTS Player (
+                player_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                amp REAL,
+                tier TEXT,
+                trait TEXT,
+                player_name TEXT,
+                season_of_origin INTEGER)
+    """
+    QUERY(query, connect, is_select=False)
+    query = """ CREATE TABLE IF NOT EXISTS Game (
+                    game_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    winning_team_id INTEGER REFERENCES Team(team_id),
+                    losing_team_id INTEGER REFERENCES Team(team_id),
+                    margin_of_victory REAL,
+                    lineup_count INTEGER,
+                    playoffs INTEGER)        
+        """
+    QUERY(query, connect, is_select=False)
+    query = """ CREATE TABLE IF NOT EXISTS Match (
+                        match_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        winning_team_id INTEGER REFERENCES Team(team_id),
+                        losing_team_id INTEGER REFERENCES Team(team_id),
+                        match_score TEXT,
+                        _0N_winner_id INTEGER REFERENCES Team(team_id),
+                        _1N_winner_id INTEGER REFERENCES Team(team_id),
+                        _2N_winner_id INTEGER REFERENCES Team(team_id),
+                        _3N_winner_id INTEGER REFERENCES Team(team_id),
+                        _4N_winner_id INTEGER REFERENCES Team(team_id),
+                        _5N_winner_id INTEGER REFERENCES Team(team_id),
+                        _6N_winner_id INTEGER REFERENCES Team(team_id),
+                        _7N_winner_id INTEGER REFERENCES Team(team_id),
+                        _8N_winner_id INTEGER REFERENCES Team(team_id),
+                        playoffs TEXT)        
+            """
+    QUERY(query, connect, is_select=False)
+    query = """ CREATE TABLE IF NOT EXISTS Series (
+                                series_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                winning_team_id INTEGER REFERENCES Team(team_id),
+                                losing_team_id INTEGER REFERENCES Team(team_id),
+                                winning_seed BLOB,
+                                losing_seed BLOB,
+                                seed_diff BLOB,
+                                winner_name TEXT,
+                                loser_name TEXT,
+                                series_score TEXT,
+                                _0N_score TEXT,
+                                _1N_score TEXT,
+                                _2N_score TEXT,
+                                _3N_score TEXT,
+                                _4N_score TEXT,
+                                _5N_score TEXT,
+                                _6N_score TEXT,
+                                _7N_score TEXT,
+                                _8N_score TEXT,
+                                series_game_score TEXT,
+                                context TEXT)        
+                    """
+    QUERY(query, connect, is_select=False)
+    query = """ CREATE TABLE IF NOT EXISTS TotalSeason (
+                                season_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                uni_champ_id INTEGER REFERENCES Team(team_id),
+                                biggest_upset TEXT,
+                                darkwing_champ_id INTEGER REFERENCES Team(team_id),
+                                shining_core_champ_id INTEGER REFERENCES Team(team_id),
+                                diamond_sea_champ_id INTEGER REFERENCES Team(team_id),
+                                web_of_nations_champ_id INTEGER REFERENCES Team(team_id),
+                                ice_wall_champ_id INTEGER REFERENCES Team(team_id),
+                                candyland_champ_id INTEGER REFERENCES Team(team_id),
+                                hells_circle_champ_id INTEGER REFERENCES Team(team_id),
+                                steel_heart_champ_id INTEGER REFERENCES Team(team_id))        
+                    """
+    QUERY(query, connect, is_select=False)
+
+
+    return 0
 
 
 def write_to_file(filename=None, words=None, mode='w', error=False):
@@ -136,6 +263,55 @@ def best_of_stats(season_stats_list, season_count, alt_stats_list=None, avg_stat
     for i in range(SMALL_RANGE):
         full_season_list[i].print_effect()
 
+    with open('best_stats', 'a') as p:
+        p.write('//////////////\n\nBEST HEALERS\n\n')
+    full_season_list.sort(key=lambda s: s.healed, reverse=True)
+    for i in range(SMALL_RANGE):
+        full_season_list[i].print_player_season('best_stats')
+
+    with open('best_stats', 'a') as p:
+        p.write('//////////////\n\nBEST REFLECTORS\n\n')
+    full_season_list.sort(key=lambda s: s.reflected, reverse=True)
+    for i in range(SMALL_RANGE):
+        full_season_list[i].print_player_season('best_stats')
+
+def finalize_series_data():
+    query = """
+        UPDATE Series
+        SET winning_seed = 
+    CASE
+        WHEN instr(winner_name, '(') = 0 OR instr(winner_name, ')') = 0 THEN CAST('n/a' AS BLOB)
+        ELSE CAST(
+                substr(
+                    winner_name, 
+                    instr(winner_name, '(') + 1, 
+                    instr(winner_name, ')') - instr(winner_name, '(') - 1
+                ) AS INTEGER
+            )
+    END;  """
+    QUERY(query)
+    query = """
+        UPDATE Series
+        SET losing_seed = 
+    CASE
+        WHEN instr(loser_name, '(') = 0 OR instr(loser_name, ')') = 0 THEN CAST('n/a' AS BLOB)
+        ELSE CAST(
+                substr(
+                    loser_name, 
+                    instr(loser_name, '(') + 1, 
+                    instr(loser_name, ')') - instr(loser_name, '(') - 1
+                ) AS INTEGER
+            )
+    END;  """
+    QUERY(query)
+    query = """
+    UPDATE Series
+    SET seed_diff = CASE
+        WHEN winning_seed LIKE '%n/a%' OR losing_seed LIKE '%n/a%' THEN CAST('n/a' AS BLOB)
+        ELSE (winning_seed - losing_seed)
+        END;
+    """
+    QUERY(query)
 
 
 def series_test(wins, losses):
